0a23f710d60fa034fa192e307dbc08234d988e27
Naveen <naveeinsingh@gmail.com>
2025-10-29 18:00:37 +0530

    --basicAPISetup-product-card-iteration

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,67 @@
-<script setup lang="ts">
+<template>
+  <AnnouncementCarousel />
+  <HeaderBar 
+      @enable-search-bar="enableSearchBar"
+  />
+  <SearchBar 
+      v-if="isSearchEnable" 
+      @close-search-bar="closeSearchBar"
+  />
+  <div v-else>
+    <CollectionBanner />
+      <TopCarousel />
+      <ExpressBanner />
+      <ProductCardBanner />
+      <CollectionToolbar />
+  </div>
+  
+  <div class="card-container flex flex-[100%] flex-wrap">
+    <div class="loading-overlay" v-if="isLoading">Loading Products...</div>
+    
+    <ProductCard 
+        v-else-if="!isLoading"
+        v-for="product in searchResult.results" 
+        :key="product.id"
+        :product-data="product"
+    />
+
+    <div class="no-results w-full" v-else-if="!isLoading">
+      <div class="container flex-col items-center flex justify-center">
+        <div class="no-result-img flex flex-col w-100 justify-center items-center"><img src="../public/image.png" alt=""></div> 
+        <h2 class="page-heading flex flex-col w-100 justify-center items-center">Sorry, we can’t find any result <span>for "{{ searchValue }}"</span></h2> 
+        <div class="search-pform flex flex-col w-100 justify-center items-center">
+          <p> Please try a different term. 
+            <span style="display: none;">or try <a class="highlight-text">clearing</a> some filters</span>
+          </p>
+        </div>
+      </div> 
+    </div>
+  </div>
+  
+  <PaginationSection />
+  <SectionFooter />
+
+  <ProductFilterSidebar />
+  <ProductCounter />
+  <WhatsappRedirect />
+  
+</template>
+
+<script lang="ts">
+
+// Imports
+import SearchClient from "@gaspl/search-client"
+import { defineComponent } from "vue";
+
+// Working NICOBAR TOKENS
+const searchToken = import.meta.env.VITE_NICOBAR_SEARCH_TOKEN as string;
+const applicationId =import.meta.env.VITE_NICOBAR_APPLICATION_ID as string;
+const collectionId =import.meta.env.VITE_NICOBAR_COLLECTION_ID as string;
+
+// Client initialization
+var searchClient = new SearchClient(applicationId, searchToken);
+
+// Componenet Imports
 import CollectionBanner from './components/CollectionBanner.vue';
 import HeaderBar from './components/HeaderBar.vue';
 import AnnouncementCarousel from './components/AnnouncementCarousel.vue';
@@ -14,37 +77,50 @@ import SearchBar from './components/SearchBar.vue';
 import WhatsappRedirect from './components/WhatsappRedirect.vue';
 import ProductCounter from './components/ProductCounter.vue';
 
+
+export default defineComponent({
+  components: {
+    CollectionBanner, HeaderBar, AnnouncementCarousel, ProductCard, TopCarousel, ExpressBanner, ProductCardBanner, CollectionToolbar, PaginationSection, SectionFooter, ProductFilterSidebar, SearchBar, WhatsappRedirect, ProductCounter
+  },
+  data() {
+    return {
+      isLoading: true as boolean,
+      searchValue: '*' as string,
+      searchResult: null as any,
+      isSearchEnable: false as boolean,
+
+    };
+  },
+  async mounted(){
+    try {
+      this.isLoading = true;
+      console.log("Componenet Mounted. Started searching");
+      const result = await searchClient.search(this.searchValue, collectionId);
+      this.searchResult = result;
+      console.log(JSON.stringify(result, null, 2));
+    } catch (error){
+      console.log("Search failed:", error);
+    }finally {
+      this.isLoading = false;
+    }
+  },
+  methods: {
+    closeSearchBar(){
+      this.isSearchEnable = false;
+    },
+    enableSearchBar(){
+      if(this.isSearchEnable === true){
+        this.isSearchEnable = false;
+      } else {
+        this.isSearchEnable = true;
+      }
+    },
+
+  }
+})
+
 </script>
 
-<template>
-  <AnnouncementCarousel />
-  <HeaderBar />
-  <SearchBar />
-  <CollectionBanner />
-  <TopCarousel />
-  <ExpressBanner />
-  <ProductCardBanner />
-  <CollectionToolbar />
-
-  <div class="card-container flex flex-[100%] flex-wrap">
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-  </div>
-  
-  <PaginationSection />
-  <SectionFooter />
-
-  <ProductFilterSidebar />
-  <ProductCounter />
-  <WhatsappRedirect />
-  
-</template>
-
 <style scoped>
+
 </style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,7 +3,7 @@
     <div class="searchbar w-full relative">
         <input type="text" class="text-[14px] font-bold border w-full min-w-[180px] bg-white bg-none bg-position-[20px] bg-no-repeat leading-[normal] text-[#22272f] pl-5 pr-[130px] py-[30px] border-solid border-black" placeholder="Search for a Product, Category.. ">
         <span class="reset-container underline absolute right-20 top-[30%] hidden">reset</span>
-        <span class="close-btn absolute right-5 top-1/3">
+        <span class="close-btn absolute right-5 top-1/3" @click="$emit('close-search-bar')">
             <img src="../../public/cross-svgrepo-com.svg" class="h-6 w-6" alt="">
         </span>
     </div>
@@ -15,5 +15,12 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
     name: 'SearchBar',
+    emits: ['update-search', 'close-search-bar'],
+    methods: {
+        handleInput(event: Event){
+            const target = event.target as HTMLInputElement;
+            this.$emit('update-search', target.value);
+        },
+    }
 })
 </script>
