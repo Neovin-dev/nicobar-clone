517f62739bc32f01201fa9e0b7b96ece3b4b4f0e
Naveen <naveeinsingh@gmail.com>
2025-11-04 13:16:45 +0530

    search-style

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="search-bar-wrapper">
     <div class="searchbar w-full relative">
-        <input @keyup.enter="handleSearch" ref="searchInput" type="text" class="text-[21px] font-thin border w-full min-w-[180px] bg-white bg-none bg-position-[20px] bg-no-repeat leading-[normal] text-[#22272f] pl-5 pr-[130px] py-[30px] border-solid border-black placeholder:text-sm placeholder:font-bold" placeholder="Search for a Product, Category.. ">
+        <input @keyup.enter="handleSearch" ref="searchInput" type="text" class="search-input text-[21px] font-thin border w-full min-w-[180px] bg-white bg-none bg-position-[20px] bg-no-repeat leading-[normal] text-[#22272f] pl-5 pr-[130px] py-[30px] border-solid border-black placeholder:text-sm placeholder:font-bold" placeholder="Search for a Product, Category.. ">
         <span @click="handleSearch" class="absolute right-20 top-[30%] border p-1.5 flex items-center"><button class="cursor-pointer">Search</button></span>
         <span class="reset-container underline absolute right-20 top-0 h-full items-center hidden">reset</span>
         <span class="close-btn absolute right-5 top-0 items-center flex h-full" @click="$emit('close-search-bar')">
@@ -31,3 +31,15 @@ export default defineComponent({
     }
 })
 </script>
+
+<style scoped>
+
+@media (max-width: 820px){
+.search-input {
+    margin: 20px;
+    padding: 10px;
+    border: 0;
+    border-bottom: 1px solid black;
+}
+}
+</style>
